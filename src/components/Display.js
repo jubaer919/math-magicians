3f75ef4c0991fa00c3e55ef445663f03fe49d775
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-06-13 19:06:27 +0600

    followed exacly the wireframe design

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,8 +3,7 @@ import './display.css';
 function Display() {
   return (
     <div className="display">
-      <div className="previous-value">756 +</div>
-      <div className="current-value">200</div>
+      <div className="current-value">200 +</div>
     </div>
   );
 }
